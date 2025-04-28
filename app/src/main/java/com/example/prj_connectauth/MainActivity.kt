6cf2c2fa41c2ae2243b62e3f0dd9b7787e2f6347
Illee_Silva <illee.silvalb@gmail.com>
2025-04-28 16:18:09 -0300

    Já nem lembro

--- app/src/main/java/com/example/prj_connectauth/MainActivity.kt
+++ app/src/main/java/com/example/prj_connectauth/MainActivity.kt
@@ -1,20 +1,77 @@
 package com.example.prj_connectauth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+
+import com.google.firebase.auth.FirebaseAuth
+import com.example.prj_connectauth.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null){
+            startActivity(Intent(this, sucess::class.java))
+            finish()
+        }
+
+        binding.bttLogin.setOnClickListener(){
+            val email = binding.txtEmail.text.toString().trim()
+            val password = binding.txtPassword.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Preencha todos os Campos!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){ task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(this, "Login bem-sucedido!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, sucess::class.java))
+                        finish()
+                    }
+                    else{
+                        Toast.makeText(this, "Erro: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+
+        binding.textView4.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        binding.textView3.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        FirebaseAuth.getInstance().signOut()
     }
 }
--- app/src/main/java/com/example/prj_connectauth/RegisterActivity.kt
+++ app/src/main/java/com/example/prj_connectauth/RegisterActivity.kt
@@ -1,20 +1,56 @@
 package com.example.prj_connectauth
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.example.prj_connectauth.databinding.ActivityRegisterBinding
+
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.bttLogin.setOnClickListener{
+            val email = binding.txtEmail2.text.toString().trim()
+            val password = binding.txtPassword2.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Preencha todos os Campos!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){ task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "Registrto bem sucedido!", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        Toast.makeText(this, "ERRO: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+
     }
+
 }
