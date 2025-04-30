2b6b1ddeea2ea1bf871151af92cd0a157f4ee855
Illee_Silva <illee.silvalb@gmail.com>
2025-04-29 22:24:00 -0300

    Tratamento de Erros Adicionais e ProgressBar adicionada

--- app/src/main/java/com/example/prj_connectauth/RegisterActivity.kt
+++ app/src/main/java/com/example/prj_connectauth/RegisterActivity.kt
@@ -2,7 +2,10 @@ package com.example.prj_connectauth
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -39,19 +42,29 @@ class RegisterActivity : AppCompatActivity() {
 
             it.isEnabled = false
 
+            binding.progressBar.visibility = View.VISIBLE
+
             val email = binding.txtEmail2.text.toString().trim()
             val password = binding.txtPassword2.text.toString().trim()
 
             if (email.isEmpty() || password.isEmpty()){
                 it.isEnabled = true
                 Snackbar.make(binding.root, "Preencha Todos os Campos!", Snackbar.LENGTH_SHORT).show()
+                binding.progressBar.visibility = View.GONE
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Snackbar.make(binding.root, "E-mail inválido!", Snackbar.LENGTH_SHORT).show()
+                it.isEnabled = true
+                binding.progressBar.visibility = View.GONE
                 return@setOnClickListener
             }
 
             auth.createUserWithEmailAndPassword(email, password)
 
                 .addOnCompleteListener(this){ task ->
-
+                    binding.progressBar.visibility = View.GONE
                     it.isEnabled = true
 
                     if(task.isSuccessful){
@@ -65,7 +78,7 @@ class RegisterActivity : AppCompatActivity() {
                 }
 
                 .addOnFailureListener { exception ->
-
+                    binding.progressBar.visibility = View.GONE
                     it.isEnabled = true
 
                     handleSignUpError(exception)
@@ -93,6 +106,9 @@ class RegisterActivity : AppCompatActivity() {
             }
             else -> {
                 // Erro genérico (ex: rede)
+                Log.e("REGISTER_ERROR", "Classe da exceção: ${exception?.javaClass?.name}")
+                Log.e("REGISTER_ERROR", "Mensagem: ${exception?.message}")
+
                 Snackbar.make(binding.root, "Erro: ${exception?.message}", Snackbar.LENGTH_SHORT).show()
             }
         }
